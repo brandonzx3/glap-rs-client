bd98a431c5c561239afb69a1885f54b58eb88668
Christian7573 <cag4561@gmail.com>
2020-08-04 14:17:59 -0500

    Switched to flatbuffer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js';
-import { FromServer, ToServer } from "./codec"
-import { encode as serialize, decode as deserialize } from "msgpack-lite";
+import { flatbuffers } from "flatbuffers";
+import { ToClient, ToServer } from "./codec";
 
 const pixi = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight, antialias: false, transparent: false});
 document.body.appendChild(pixi.view);
@@ -9,17 +9,27 @@ document.body.appendChild(pixi.view);
 const socket = new WebSocket(decodeURIComponent(window.location.hash.slice(1)));
 socket.binaryType = "arraybuffer";
 socket.onopen = () => {
-    let handshake = { "client": "glap.rs-0.1.0", "session": null as null };
-    let msg: any = {};
-    msg[ToServer.to_id.get(ToServer.Handshake)] = handshake;
-    socket.send(serialize(msg));
+    const builder = new flatbuffers.Builder();
+    const client = builder.createString("glap.rs-0.1.0");
+    ToServer.Handshake.startHandshake(builder);
+    ToServer.Handshake.addClient(builder, client);
+    //ToServer.Handshake.addSession(builder, null);
+    const handshake = ToServer.Handshake.endHandshake(builder);
+    ToServer.Msg.startMsg(builder);
+    ToServer.Msg.addMsgType(builder, ToServer.ToServerMsg.Handshake);
+    ToServer.Msg.addMsg(builder, handshake);
+    const msg = ToServer.Msg.endMsg(builder);
+    builder.finish(msg);
+    socket.send(builder.asUint8Array());
 };
 socket.onmessage = (e) => {
     try {
-        const msg = deserialize(new Uint8Array(e.data));
-        if (FromServer.from_id.get(Object.keys(msg)[0]) !== FromServer.HandshakeAccepted) throw new Error();
+        const buf = new flatbuffers.ByteBuffer(new Uint8Array(e.data));
+        const acceptance = ToClient.Msg.getRootAsMsg(buf);
+        if (acceptance === null || acceptance === undefined) throw new Error();
+        console.log(acceptance.msgType());
         (document.querySelector("#connecting_msg") as HTMLDivElement).style.display = "none";
-        alert("Connected!");
+        alert("Accepted");
     } catch(e) {
         document.querySelector("#connecting_msg span").innerHTML = "Server commited die or something";
         console.error(e);
