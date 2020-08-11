317346faaf65c7d3f39c5ec26281cb2e3b02162d
Christian7573 <cag4561@gmail.com>
2020-08-11 12:46:34 -0500

    Rendering in progress

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as PIXI from 'pixi.js';
-import { ToClientMsg, ToServerMsg, Box } from "./codec";
+import { ToClientMsg, ToServerMsg, Box, PartKind } from "./codec";
 
 const pixi = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight, antialias: false, transparent: false, backgroundColor: 0 });
 document.body.appendChild(pixi.view);
@@ -7,6 +7,7 @@ document.body.appendChild(pixi.view);
 const scaling = new PIXI.Container();
 const world = new PIXI.Container();
 scaling.addChild(world);
+pixi.stage.addChild(scaling);
 let scale_up;
 function resize() {
     const window_size = Math.min(window.innerWidth, window.innerHeight);
@@ -17,6 +18,8 @@ function resize() {
     scaling.scale.set(scale_up, scale_up);
 }
 resize();
+
+let rendering = true;
 
 let spritesheet: PIXI.Spritesheet;
 new Promise(async (resolve, reject) => {
@@ -51,12 +54,41 @@ new Promise(async (resolve, reject) => {
     socket.addEventListener("message", handshake_ing);
     socket.onerror = err => { throw err; };
 
+    let my_core: PIXI.Sprite = null;
+    const parts = new Map<number, PIXI.Sprite>();
+    const celestial_objects = new Map<number, PIXI.Sprite>();
+
     function on_message(e: MessageEvent) {
         const msg = ToClientMsg.deserialize(new Uint8Array(e.data), new Box(0));
 
         if (msg instanceof ToClientMsg.AddCelestialObject) {
-            
+            const celestial_object = new PIXI.Sprite(spritesheet.textures[msg.name + ".png"]);
+            celestial_object.width = msg.radius * 2;
+            celestial_object.height = msg.radius * 2;
+            celestial_object.position.set(msg.position[0], msg.position[1]);
+            world.addChild(celestial_object);
+            celestial_objects.set(msg.id, celestial_object);
+        }
+
+        else if (msg instanceof ToClientMsg.AddPart) {
+            const part = new PIXI.Sprite(spritesheet.textures[PartKind[msg.kind] + ".png"]);
+            part.width = 1; part.height = 1;
+            world.addChild(part);
+            parts.set(msg.id, part);
+        }
+        else if (msg instanceof ToClientMsg.MovePart) {
+            const part = parts.get(msg.id);
+            part.position.set(msg.x, msg.y);
+            part.rotation = Math.atan2(msg.rotation_i, msg.rotation_n);
         }
     }
 
+    function render() {
+        if (rendering) {
+            //if (my_core != null) { world.position.set(-my_core.position.x, -my_core.position.y); }
+            pixi.render();
+            requestAnimationFrame(render);
+        }
+    }
+    requestAnimationFrame(render);
 });
